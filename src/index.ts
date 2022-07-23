a1aad67ed143470818722ae9d0523541d9549e16
twpDiogenes <twisepigeon@gmail.com>
2022-07-23 13:58:54 +0000

    added port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import express from "express";
 import { Request, Response } from "express";
 
 const app = express();
+const port = 3000 || process.env.PORT
+
 
 app.get('/', (req: Request, res: Response)=>{
     res.send('Hello World!');
+})
+
+
+app.listen(port, ()=>{
+    console.log('I am listening nigga')
 })
